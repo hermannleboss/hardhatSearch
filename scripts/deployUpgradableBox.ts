8a05a83e53e1650276dbb85d37798fc2bc381495
Hermann EHO <hermannleboss@gmail.com>
2024-07-24 10:07:12 +0000

    feat: upgradable box deploy script

---
+++ scripts/deployUpgradableBox.ts
@@ -0,0 +1,19 @@
+import { ethers, upgrades } from "hardhat";
+
+// scripts/deploy.js
+async function main() {
+  // We get the contract to deploy
+  const Box = await ethers.getContractFactory("Box");
+  console.log("Deploying Upgradable Box...");
+
+  const box = await upgrades.deployProxy(Box, [42], { initializer: "store" });
+  // const box = await Box.deploy();
+  await box.waitForDeployment();
+  console.log("Upgradable Box deployed to:", await box.getAddress());
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
